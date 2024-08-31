77bc5adead080a46de240e755044e90f9b8d0a3d
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-08-31 23:19:19 +0300

    Add release.sh build script

---
+++ release.sh
@@ -0,0 +1,30 @@
+#/bin/sh
+
+OUTDIR=x1e-packages
+REPO=linux-x1e
+
+if [[ $1 != "--skip-build" ]]; then
+    echo 'Building packages...'
+    for pkg in "linux-x1e" "x1e-firmware" "x1e-keyring" ; do
+        cd $pkg
+        PKGEXT=.pkg.tar.xz CARCH=aarch64 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- nice -19 makepkg -sCA
+        cd ..
+    done
+fi
+
+[[ -d $OUTDIR ]] && rm -rf $OUTDIR
+mkdir $OUTDIR && cd $OUTDIR
+
+cp ../*/*.pkg.tar.xz ./
+
+for pkg in *.pkg.tar.xz; do
+    gpg --detach-sign "${pkg}"
+done
+
+repo-add --sign $REPO.db.tar.gz *.pkg.tar.xz
+
+git tag -d packages && git tag packages && git push -f --tags
+
+yes | gh release delete packages
+gh release create packages --notes ""
+gh release upload packages *
